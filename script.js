aa13844b8e77dfe6def9a675e57f36a90c12bcd2
Raymond Ha <raymond@shraymonks.com>
2015-03-05 20:11:28 -0500

    Adapt to subreddit margins

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
 var isHidden = false;
+var style = document.createElement('style');
+// Key used to store styles for this page; either r/subreddit or the root pathname
+var page = (function() {
+  var path = location.pathname.split('/');
+  return path[1] === 'r' ? 'r/' + path[2] : path[1];
+}());
+var pageStyle = {};
+var pageStyles = {};
+
+var createRule = function(selector, elemStyle) {
+  var rule = selector + '{';
+
+  for (var prop in elemStyle) {
+    rule += prop + ':' + elemStyle[prop] + ' !important;';
+  }
+  return rule + '}';
+};
+
+var insertRule = function(elem, elemStyle) {
+  if (Object.keys(elemStyle).length === 0) return;
+
+  // Redundant selectors like `.side.side` are to increase specificity
+  // to safeguard against subreddits that use `!important`
+  var selector;
+  switch (elem) {
+    case 'main':
+      selector = '.content[role="main"]';
+      break;
+    case 'side':
+      selector = '.side.side';
+      break;
+    case 'siteTable':
+      selector = '#siteTable#siteTable';
+      break;
+  }
+  style.sheet.insertRule(createRule(selector, elemStyle), style.sheet.length);
+};
 
 // Setup sidebar CSS with initial toggle setting
 //
 // This is done here with an injected <style> instead of
 // adding a class to .side to prevent any reflow or flash
 // while initially hiding the sidebar
-chrome.storage.local.get({isHidden: false}, function(items) {
-  var style = document.createElement('style');
-
+chrome.storage.local.get({isHidden: false, pageStyles: {}}, function(items) {
   isHidden = items.isHidden;
 
   // style.sheet gets created after style is inserted into the DOM
@@ -16,9 +51,18 @@ chrome.storage.local.get({isHidden: false}, function(items) {
   var sidebarSelector = isHidden ? '.side:not(.toggle)' : '.side.toggle';
 
   style.sheet.insertRule(sidebarSelector + ' {display: none}', 0);
+
+  pageStyles = items.pageStyles;
+  pageStyle = pageStyles[page];
+
+  // Match margins of the subreddit theme
+  if (pageStyle) {
+    for (var elem in pageStyle) {
+      insertRule(elem, pageStyle[elem]);
+    }
+  }
 });
 
-// Create the toggle link
 document.addEventListener('DOMContentLoaded', function() {
   var side = document.getElementsByClassName('side')[0];
 
@@ -27,9 +71,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
   var header = document.getElementById('header-bottom-right');
   var fragment = document.createDocumentFragment();
+  var main = document.querySelector('[role="main"]');
   var separator = header.getElementsByClassName('separator')[0].cloneNode(true);
+  var siteTable = document.getElementById('siteTable');
   var toggle = document.createElement('a');
 
+  // Create the toggle link
   toggle.className = 'toggle-sidebar';
   toggle.innerText = 'Toggle sidebar';
   toggle.addEventListener('click', function() {
@@ -41,4 +88,55 @@ document.addEventListener('DOMContentLoaded', function() {
   fragment.appendChild(separator);
   fragment.appendChild(toggle);
   header.appendChild(fragment);
+
+
+  // Match subreddit theme margins
+  if (!pageStyle) {
+    pageStyles[page] = {};
+    pageStyle = pageStyles[page];
+  }
+
+  var newStyles = false;
+
+  // Theme margins are stored so that they can be applied before the DOM is loaded
+  // on subsequent page loads, elimintating reflow
+  var mainStyle = getComputedStyle(main);
+  if (mainStyle.marginLeft !== mainStyle.marginRight) {
+    pageStyle.main = pageStyle.main || {};
+    pageStyle.main['margin-right'] = mainStyle.marginLeft;
+    insertRule('main', {'margin-right': mainStyle.marginLeft});
+    newStyles = true;
+  }
+
+  var sideStyle = getComputedStyle(side);
+  var sideMarginLeft = Math.max(
+    parseInt(sideStyle.marginRight, 10),
+    parseInt(mainStyle.marginLeft, 10)
+  ) + 'px';
+  if (sideStyle.marginLeft !== sideMarginLeft) {
+    pageStyle.side = pageStyle.side || {};
+    pageStyle.side['margin-left'] = sideMarginLeft;
+    insertRule('side', {'margin-left': sideMarginLeft});
+    newStyles = true;
+  }
+
+  if (siteTable) {
+    var siteTableStyle = getComputedStyle(siteTable);
+    if (siteTableStyle.marginLeft !== siteTableStyle.marginRight) {
+      pageStyle.siteTable = pageStyle.siteTable || {};
+      pageStyle.siteTable['margin-right'] = siteTableStyle.marginLeft;
+      insertRule('siteTable', {'margin-right': siteTableStyle.marginLeft});
+      newStyles = true;
+    }
+    if (siteTableStyle.paddingLeft !== siteTableStyle.paddingRight) {
+      pageStyle.siteTable = pageStyle.siteTable || {};
+      pageStyle.siteTable['padding-right'] = siteTableStyle.paddingLeft;
+      insertRule('siteTable', {'padding-right': siteTableStyle.paddingLeft});
+      newStyles = true;
+    }
+  }
+
+  if (newStyles) {
+    chrome.storage.local.set({pageStyles: pageStyles});
+  }
 });
